2c25cc79ad44ad2b67033ab1a35f054625e485c9
Jocelyn Llamas <mjlldlt@hotmail.com>
2024-06-25 13:02:06 -0600

    list users feature

--- src/app/pages/users-list/users-list.component.ts
+++ src/app/pages/users-list/users-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/users/user.service';
 
 @Component({
   selector: 'app-users-list',
@@ -6,11 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-list.component.css']
 })
 export class UsersListComponent implements OnInit {
-  creditRequests: any[] = [];
-  
-  constructor() { }
+
+  users: User[] = [];
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.getUsers().subscribe(users => {
+      this.users = users;
+    });
   }
 
 }
