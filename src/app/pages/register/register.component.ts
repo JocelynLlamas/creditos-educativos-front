5e9db94011211cf477ff2a92cc3b2131eeb83439
Jocelyn Llamas <mjlldlt@hotmail.com>
2024-06-25 12:55:41 -0600

    Add user feature

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/users/user.service';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-register',
@@ -11,11 +13,13 @@ export class RegisterComponent {
   username: string = '';
   password: string = '';
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, private userService: UserService) { }
 
   register() {
-    this.authService.register({ username: this.username, password: this.password }).subscribe(() => {
-      this.router.navigate(['/login']);
+    const newUser: User = { id: 0, username: this.username, password: this.password };
+
+    this.userService.createUser(newUser).subscribe(() => {
+      this.router.navigate(['/users']);
     });
   }
 }
--- src/app/services/users/user.service.ts
+++ src/app/services/users/user.service.ts
@@ -10,8 +10,12 @@ export class UserService {
   private apiUrl = 'http://localhost:8080/api/users';
 
   constructor(private http: HttpClient) { }
-
-  getUsers(): Observable<User[]> { 
+  // GET
+  getUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.apiUrl);
+  }
+  // POST
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>(this.apiUrl, user);
   }
 }
